934ea93efb4e6cf5adca64a01655a7ddbee3c301
MatijosiusGitHub <arnoldas.matijosius@gmail.com>
2022-03-22 21:32:22 +0200

    3nd commit

--- JS atsiskaitymas/JS atsiskaitymas_mano/Task 1/Script.js
+++ JS atsiskaitymas/JS atsiskaitymas_mano/Task 1/Script.js
@@ -1,10 +1,50 @@
-document.getElementById(SvorioIsvedimas).addEventListener("submit-btn")
+let kgbtn = document.getElementById('kg');
+let pound = document.getElementById('pound');
+let ounces = document.getElementById('ounce')
+
+    kgbtn.addEventListener('click', function () {
+    let input = document.getElementById('input',value)
+    document.getElementById('output').value = input/2.205+"kg";
+    })
+    poundbtn.addEventListener('click', function () {
+    let input = document.getElementById('input',value)
+        document.getElementById('output').value = input * 2.205 + "pound";
+    })
+    ouncesbtn.addEventListener('click', function () {
+    let input = document.getElementById('input', value)
+    document.getElementById('output').value = input * 0.035274 + "ounce";
+    })
+        
+        
+        
+/*kitos funkcijos kodas su parametrais konvertavimo formulei idiegti, 
+ir kintamasis Let su svoriu santykiu*/
+function convert_pound(kg) {
+let rate = 2.2046, pounds;
+    pounds = kg * rate;
+    /*kad gautume html faile rezultaa, paduodame jam koda*/
+    document.getElementById('output').innerHTML = `${kg}kilograms=${kilograms.toFixed(3)}kilograms.`;
+}
+function convert_ounces(kg) {
+    let rate = 0.035274, ounces;
+    ounces = kg * rate;
+    document.getElementById('output').innerHTML = `${ounces}ounces=${ounces.toFixed(3)}ounces.`;
+}
+    
+function convert_kilograms(kg) {
+    let rate = 0.0022046, kilograms;
+    pound = kg / rate;
+    /*kad gautume html faile rezultata, paduodame jam koda*/
+    document.getElementById('output').innerHTML = `${Pounds}Pounds=${Pounds.toFixed(3)}pounds.`;
+}
+document.addEventListener("click", submit - btn);
     e.preventDefault();
-let submitBtn = document.getElementById("submit-btn");
-    /*kai mygtuka Btn  paspaudziame pelyte, matome narsykleje 
-    rezultata keitimo,ty funkcija function*/
-button.addEventListener('click'), function(){
-    const kg = parseInt(document.getElementById)('kilograms').value;
+    let submit= document.getElementById("submit-btn");
+/*kai mygtuka Btn  paspaudžiame pelyte, matome naršyklėje 
+rezultata keitimo,ty funkcija function*/
+button.addEventListener('click'), function ()
+{
+    const kg = parseInt(document.getElementById)('Kilograms').value;
 };
     const type = document.getElementById('type').value;
 /* ciklas naudojamas patikrinti ar ne tuscias ivesties langelis*/
@@ -16,7 +56,7 @@ if (kg === 'isNaN'(kg)) {
 else {
     document.getElementById('error').innerHTM= ``;
 }
-    switch (type) {
+switch (type) {
     case 'Pounds':
         /*iskvieciu funkcija, kad paverstų svorį į svarus, nurodydamas tokius parametrus kaip Value*/
         convert_pounds(kg);
@@ -31,30 +71,4 @@ else {
         convert_pounds(kg)
         break;
     default:
-        allert('Error')
-    }
-/*kitos funkcijos kodas su parametrais konvertavimo formulei idiegti, 
-ir kintamasis Let su svoriu santykiu*/
-
-function convert_pounds(kg) {
-    let rate = 2.2046, pounds;
-    pounds = kg * rate;
-    /*kad gautume html faile rezultaa, paduodame jam koda*/
-    document.getElementById('output').innerHTML = `${kilograms}kilograms=${kilograms.toFixed(3)}kilograms.`;
-}
-    function convert_ounces(kg) {
-        let rate = 0.035274, ounces;
-        ounces = kg * rate;
-        document.getElementById('output').innerHTML = `${ounces}ounces=${ounces.toFixed(3)}ounces.`;
-    }
-    
-    function convert_kilograms(kg) {
-        let rate = 0.0022046, kilograms;
-        pound = kg / rate;
-        /*kad gautume html faile rezultata, paduodame jam koda*/
-        document.getElementById('output').innerHTML = `${pounds}pounds=${pounds.toFixed(3)}pounds.`;
-}
-e.preventDefault();
-    
-   
-    
+        allert('Error')}
